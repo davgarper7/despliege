7803fb3bacf8e468644a20b0afa8ecdcfe748d9e
davgarper7 <davgarper7@alu.edu.gva.es>
2025-02-02 21:28:02 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -87,7 +87,7 @@ app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
 
-// EvoUsers - Registro de peticiones HTTP
+// EvoUsers - Registro de peticiones http
 app.use((req, res, next) => {
   console.log(`Petición: ${req.method} ${req.url}`);
   next();
